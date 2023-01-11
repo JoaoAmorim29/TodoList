b729ab3f75d4e6c2792e70d41e31def83b50d1fd
JoaoAmorim29 <joaovitor.amorim2912@gmail.com>
2023-01-11 00:07:46 -0300

    Resolvendo bug de tamanho de caracteres para diferentes tamanhos de tela

--- js/validate.js
+++ js/validate.js
@@ -1,12 +1,23 @@
 function validate(campo){
     let errors = [];
+    let windowWidth = window.innerWidth
     
     if(campo.length == 0){
         errors.push("Campo obrigatório");
     }
 
-    if(campo.length > 25){
-        errors.push("Campo só pode conter no máximo 25 caracteres");
+    if (windowWidth >= 400){
+        if(campo.length > 25){
+            errors.push("Campo só pode conter no máximo 25 caracteres");
+        }
+    } else if (windowWidth >= 350){
+        if(campo.length > 15){
+            errors.push("Campo só pode conter no máximo 15 caracteres");
+        }
+    } else if (windowWidth >= 300){
+        if(campo.length > 12){
+            errors.push("Campo só pode conter no máximo 12 caracteres");
+        }
     }
 
     return errors;
